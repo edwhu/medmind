d15d77adb86e2bd523982ff010d99af389a512f9
Daniel Koo <koodanie@usc.edu>
2018-09-11 00:24:34 -0700

    draft of splash screen according to design on marvelapp

---
+++ src/screens/SplashScreen/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { medmindBlue } from '../../constants/styles';
+
+export default styles = StyleSheet.create({
+  container:{
+    backgroundColor: medmindBlue,
+    height: '100%',
+  },
+  logo: {
+    height: 100,
+    margin: 'auto',
+    marginTop: '40%'
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    paddingTop: '10%',
+    fontSize: 50,
+  }
+
+});
