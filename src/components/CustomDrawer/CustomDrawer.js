a7615bbaf6ffa986761b09db03a6fd37c50b31c9
Daniel Koo <koodanie@usc.edu>
2018-09-25 15:44:39 -0700

    some comments

--- src/components/CustomDrawer/CustomDrawer.js
+++ src/components/CustomDrawer/CustomDrawer.js
@@ -9,21 +9,21 @@ import styles from './styles';
 
 
 export default class CustomDrawer extends Component {
-  static propTypes = {
-    // title: PropTypes.string,
-    // hasMenu: PropTypes.bool,
-    // hasSettings: PropTypes.bool,
-  };
-
-  static defaultProps = {
-    // title: 'Medmind',
-    // hasMenu: true,
-    // hasSettings: true,
-  };
-
-  state = {
-
-  };
+  // static propTypes = {
+  //   // title: PropTypes.string,
+  //   // hasMenu: PropTypes.bool,
+  //   // hasSettings: PropTypes.bool,
+  // };
+  //
+  // static defaultProps = {
+  //   // title: 'Medmind',
+  //   // hasMenu: true,
+  //   // hasSettings: true,
+  // };
+  //
+  // state = {
+  //
+  // };
 
   render() {
     return (
@@ -42,43 +42,3 @@ export default class CustomDrawer extends Component {
   }
 
 }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     height: 80,
-//     backgroundColor: medmindBlue,
-//     width: '100%',
-//   },
-//   appBar: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     marginLeft: 15,
-//     marginRight: 10,
-//   },
-//   hamburgerWrapper: {
-//     width: 32,
-//     height: 32,
-//     alignItems: 'center',
-//     flex: 1,
-//   },
-//   hamburgerIcon: {
-//     alignSelf: 'flex-start'
-//   },
-//   settingsWrapper: {
-//     width: 32,
-//     height: 32,
-//     alignItems: 'center',
-//     flex: 1,
-//   },
-//   settingsIcon: {
-//     alignSelf: 'flex-end',
-//   },
-//   appBarTitle: {
-//     color: 'white',
-//     fontWeight: '500',
-//     fontFamily: 'System',
-//     fontSize: 24,
-//     flex: 1,
-//   }
-// })
