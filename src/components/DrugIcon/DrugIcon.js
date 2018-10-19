9e2c47402e2efdecae447dcdf153ad491b81bf6b
Daniel Koo <koodanie@usc.edu>
2018-10-18 17:29:37 -0700

    Day view (#10)
    * Almost done with day view, need some tweaks and actual data binding
    
    * colors are dynamically changed
    
    * background of drug in events changes to half opacity of the drug color
    
    * done with day view, reorganized some code, added component DrugIcon for everyone to use with two props color and scale
    
    * style issues

---
+++ src/components/DrugIcon/DrugIcon.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, TouchableHighlight, Text, Image } from 'react-native';
+
+const DrugIcon = ({ color , scale=1}) => {
+  let styles = createStyles(color, scale);
+  return <View style={styles.pillIconContainer}>
+          <View style={styles.pillColorSide}></View>
+          <View style={styles.pillGraySide}></View>
+        </View>
+}
+
+const createStyles = (drugColor, scale) => {
+  return StyleSheet.create({
+    pillIconContainer: {
+      transform: [{ rotate: '-70deg'}],
+      flexDirection: "row",
+      margin: "auto",
+      shadowColor: '#000',
+      shadowOffset: { width: -2 , height: 2 },
+      shadowOpacity: 0.3,
+      shadowRadius: 2,
+    },
+    pillGraySide: {
+      height: 12.0 * scale,
+      width: 15.6 * scale,
+      borderTopRightRadius: 7.0 * scale,
+      borderBottomRightRadius: 7.0 * scale,
+      backgroundColor: "#FFFFFF",
+    },
+    pillColorSide: {
+      height: 12.0 * scale,
+      width: 15.6 * scale,
+      borderTopLeftRadius: 7.0 * scale,
+      borderBottomLeftRadius: 7.0 * scale,
+      backgroundColor: drugColor,
+    },
+  });
+}
+export default DrugIcon;
