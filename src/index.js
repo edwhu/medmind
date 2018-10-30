57576c86fdae867edf055e8d5b72e8379eeda2c9
ethan-yu0503 <ethanyu.code@gmail.com>
2018-10-30 09:34:03 -0700

    Setting up notifications
    Adding the backbones to notification setup. Need to discuss about where to retrieve the information!

--- src/index.js
+++ src/index.js
@@ -6,7 +6,47 @@ import AppNavigator from "./routes";
 import store from "./redux/store";
 YellowBox.ignoreWarnings(["Remote debugger"]); // ignore image loader warning
 
+import {Constants, Notifications, Permissions} from 'expo';
+
 export default class App extends React.Component {
+
+  fireNotification(e) {
+        const localNotification = {
+            title: 'Time to take your drugs',
+            body: 'done!' //We can add more detailed description based on the object we pass
+            /*
+              Can add more attributes
+            */
+        };  
+
+        const schedulingOptions = {
+            time: (new Date()).getTime() + Number(1000) /* Specify time needed to get notifications */
+        }
+
+        Notifications.scheduleLocalNotificationAsync(
+            localNotification, schedulingOptions
+        );
+  }
+
+  handleNotification() {
+
+        console.warn('Got Notification!');
+  }
+
+  async componentDidMount() {
+        // We need to ask for Notification permissions for ios devices
+        let result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+        if (Constants.isDevice && result.status === 'granted') {
+            console.log('Notification permissions granted.')
+        }
+
+        // If we want to do something with the notification when the app
+        // is active, we need to listen to notification events and 
+        // handle them in a callback
+        Notifications.addListener(this.handleNotification);
+  }
+
   render() {
     return (
       <View style={{ height: "100%" }}>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,9 @@
-import { createDrawerNavigator, createStackNavigator } from "react-navigation";
-import LoginScreen from "../screens/Login/Login";
-import TimelineScreen from "../screens/Timeline/Timeline";
-import CustomDrawer from "../components/CustomDrawer/CustomDrawer";
-import TermsAndConditionsScreen from "../screens/TermsConditions/TermsConditions";
-import PrivacyPolicyScreen from "../screens/PrivacyPolicy/PrivacyPolicy";
-import ReminderScreen from '../screens/ReminderScreen/ReminderScreen';
-import { medmindBlue } from "../constants/styles";
-import DayViewScreen from "../screens/DayViewScreen/DayViewScreen";
-import GlobalDrugListScreen from "../screens/GlobalDrugListScreen/GlobalDrugListScreen";
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import LoginScreen from '../screens/Login/Login';
+import TimelineScreen from '../screens/Timeline/Timeline';
+import TermsAndConditionsScreen from '../screens/TermsConditions/TermsConditions';
+import PrivacyPolicyScreen from '../screens/PrivacyPolicy/PrivacyPolicy';
+import { medmindBlue } from '../constants/styles';
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
 import ShareDrugScreen from "../screens/ShareDrugScreen/ShareDrugScreen";
 
@@ -20,30 +16,18 @@ const DrawerNavigation = createDrawerNavigator(
       screen: LoginScreen
     },
     termsAndConditionsScreen: {
-      screen: TermsAndConditionsScreen
+      screen: TermsAndConditionsScreen,
     },
     splashScreen: {
       screen: SplashScreen
     },
     shareDrugScreen: {
       screen: ShareDrugScreen
-    },
-    privacyPolicyScreen: {
-      screen: PrivacyPolicyScreen
-    }, 
-    reminderScreen: {
-      screen: ReminderScreen
-    },
-    dayViewScreen: {
-      screen: DayViewScreen
-    },
-    globalDrugListScreen: {
-      screen: GlobalDrugListScreen
     }
   },
   {
-    initialRouteName: "timelineScreen",
-    contentComponent: CustomDrawer
+    initialRouteName: "timelineScreen"
+    // contentComponent: DrawerComponent
   }
 );
 
