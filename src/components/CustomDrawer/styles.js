8aeb1b9f8c55688ef1a46a2f9b4ec67fc5781d8b
ethan-yu0503 <ethanyu.code@gmail.com>
2018-11-11 14:24:09 -0800

    Added two borders to profile picture
    Created container to create a second border – this container has slightly bigger dimensions. Added borderWidth to profile picture to create second border.

--- src/components/CustomDrawer/styles.js
+++ src/components/CustomDrawer/styles.js
@@ -2,10 +2,24 @@ import { StyleSheet } from "react-native";
 
 
 export default (styles = StyleSheet.create({
+  // profilePictureOuterContainer: {
+  //   width: 80,
+  //   height: 80,
+  //   borderRadius: 40,
+  //   borderWidth: 3
+  // },
+  profilePictureContainer: {
+    width: 76,
+    height: 76,
+    borderRadius: 38,
+    borderWidth: 3
+  },
   profilePicture: {
     width: 70,
     height: 70,
-    borderRadius: 70,
+    borderRadius: 35,
+    borderColor: "#CDF0EF",
+    borderWidth: 3
   },
   text: {
     // textAlign: "center",
