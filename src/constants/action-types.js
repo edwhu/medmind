d79d00a6c167d1f0f36e72991073b7df1794ce3e
Quinn Ngo <quinnngo@usc.edu>
2019-01-21 01:55:34 -0800

    refactor everything

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,8 +1,4 @@
-export const UPDATE_MONTH = "UPDATE_MONTH";
-export const UPDATE_YEAR = "UPDATE_YEAR";
-export const GET_PREVIOUS_WEEK = "GET_PREVIOUS_WEEK";
-export const GET_NEXT_WEEK = "GET_NEXT_WEEK";
-export const UPDATE_CURRENT_WEEK = "UPDATE_CURRENT_WEEK";
+export const UPDATE_WEEK = "UPDATE_WEEK";
 export const ADD_DRUG = "ADD_DRUG";
 export const ADD_REMINDER = "ADD_REMINDER";
 export const UPDATE_REMINDER = "UPDATE_REMINDER";
--- src/redux/reducers/timeline.js
+++ src/redux/reducers/timeline.js
@@ -1,99 +1,25 @@
 import moment from "moment";
-import {
-  UPDATE_MONTH,
-  UPDATE_YEAR,
-  GET_PREVIOUS_WEEK,
-  GET_NEXT_WEEK,
-  UPDATE_CURRENT_WEEK
-} from "../../constants/action-types";
+import { UPDATE_WEEK } from "../../constants/action-types";
 
 const now = moment();
 const start = now.clone().startOf("isoWeek");
-const end = now.clone().endOf("isoWeek");
-let weeks, newWeeks;
+
 let timelineState = {
   currentMonth: now.month(),
   currentYear: now.year(),
-  weeks: {
-    "-1": {
-      beginning: start.clone().subtract(7, "day"),
-      end: start.clone().subtract(1, "day")
-    },
-    "0": {
-      beginning: start,
-      end: end
-    },
-    "1": {
-      beginning: end.clone().add(1, "day"),
-      end: end.clone().add(7, "day")
-    }
-  }, // store the weeks as an object where 0 is the current week
-  pageTracker: ["-1", "0", "1"], // this is to help track the week swiper
-  currentWeek: "0" // should be the key in weeks
+  currentWeek: start,
 };
 
 export default (timelineReducer = (state = timelineState, action) => {
   switch (action.type) {
-    case UPDATE_MONTH:
-      state = Object.assign({}, state, { currentMonth: action.month });
-      return state;
-    case UPDATE_YEAR:
-      state = Object.assign({}, state, { currentYear: action.year });
-      return state;
-    case UPDATE_CURRENT_WEEK:
-      state = Object.assign({}, state, { currentWeek: action.currentWeek });
-      return state;
-    case GET_PREVIOUS_WEEK: {
-      // Saving all the weeks is causing the swiper to re-render every saved week, it'll be faster just to render 3 at a time
-      // weeks = state.weeks;
-      // newWeeks = Object.assign({}, weeks, {
-      //   [action.weekIndex]: action.week
-      // });
-      // state = Object.assign({}, state, {
-      //   weeks: newWeeks,
-      //   pageTracker: action.pageTracker,
-      //   currentWeek: (action.weekIndex + 1).toString()
-      // });
-      weeks = state.weeks;
-      pageTracker = state.pageTracker;
-      delete weeks[pageTracker.pop()]; // delete the last index and remove it from pageTracker
-      newWeeks = Object.assign({}, weeks, {
-        [action.weekIndex]: action.week
-      });
-      pageTracker.unshift(action.weekIndex.toString());
-      state = Object.assign({}, state, {
-        weeks: newWeeks,
-        pageTracker,
-        currentWeek: (action.weekIndex + 1).toString()
+    case UPDATE_WEEK:
+      const { currentWeek } = action;
+      state = Object.assign({}, state, { 
+        currentWeek: currentWeek,
+        currentMonth: currentWeek.month(),
+        currentYear: currentWeek.year(),
       });
       return state;
-    }
-    case GET_NEXT_WEEK: {
-      // Same as above, just save 3 at a time
-      // weeks = state.weeks;
-      // newWeeks = Object.assign({}, weeks, {
-      //   [action.weekIndex]: action.week
-      // });
-      // state = Object.assign({}, state, {
-      //   weeks: newWeeks,
-      //   pageTracker: action.pageTracker,
-      //   currentWeek: (action.weekIndex - 1).toString()
-      // });
-      // return state;
-      weeks = state.weeks;
-      pageTracker = state.pageTracker;
-      delete weeks[pageTracker.shift()];
-      newWeeks = Object.assign({}, weeks, {
-        [action.weekIndex]: action.week
-      });
-      pageTracker.push(action.weekIndex.toString());
-      state = Object.assign({}, state, {
-        weeks: newWeeks,
-        pageTracker,
-        currentWeek: (action.weekIndex - 1).toString()
-      });
-      return state;
-    }
     default:
       return state;
   }
