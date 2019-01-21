c68e1a71ad809f6ea33eb6fe7aa6ad074dcfd8e3
Quinn Ngo <quinnngo@usc.edu>
2019-01-21 02:08:37 -0800

    remove unused imports

--- src/screens/Timeline/Timeline.js
+++ src/screens/Timeline/Timeline.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, ScrollView, StyleSheet, Text, Image } from "react-native";
+import { View, Image } from "react-native";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import Swiper from "react-native-swiper";
 import ScreenHeader from "../../components/ScreenHeader/ScreenHeader";
 import WeekIcon from "../../assets/01-Week.png";
-// import CalendarWeek from '../../components/CalendarWeek/CalendarWeek';
 import WeekSwiper from "./WeekSwiper/WeekSwiper";
 import { MONTHS } from "../../constants/constants";
 import styles from "./styles";
-import moment from "moment";
 
 class TimelineScreen extends Component {
   static navigationOptions = {
