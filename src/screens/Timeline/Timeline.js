0db29cf5211de505ad463c6a1365560b00ed4e18
bhumbash <bhumbla@usc.edu>
2019-01-24 18:49:36 -0800

    Fixed minor error in Timeline.js

--- src/screens/Timeline/Timeline.js
+++ src/screens/Timeline/Timeline.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-<<<<<<< HEAD
 import { View, ScrollView, StyleSheet, Text, Image, } from "react-native";
-=======
 import { View, Image } from "react-native";
->>>>>>> 796bc2d8a4751a2af148e2dcfe4123648b718019
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import ScreenHeader from "../../components/ScreenHeader/ScreenHeader";
@@ -12,11 +9,8 @@ import WeekIcon from "../../assets/01-Week.png";
 import WeekSwiper from "./WeekSwiper/WeekSwiper";
 import { MONTHS } from "../../constants/constants";
 import styles from "./styles";
-<<<<<<< HEAD
 import moment from "moment";
 import RoundedButton from "../../components/RoundedButton/RoundedButton";
-=======
->>>>>>> 796bc2d8a4751a2af148e2dcfe4123648b718019
 
 class TimelineScreen extends Component {
   static navigationOptions = {
@@ -31,20 +25,11 @@ class TimelineScreen extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     const {navigation} = this.props;
     return (
       <View style={styles.container}>
         <ScreenHeader {...this.props} title={this.state.title} />
         <WeekSwiper navigation = {navigation}/>
-=======
-    const { currentMonth, currentYear } = this.props;
-    const title = `${MONTHS[currentMonth].toUpperCase()} ${currentYear}`;
-    return (
-      <View style={styles.container}>
-        <ScreenHeader {...this.props} title={title} />
-        <WeekSwiper />
->>>>>>> 796bc2d8a4751a2af148e2dcfe4123648b718019
       </View>
     );
   }
