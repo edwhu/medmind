f5d831bd6b1fadeb01a73b87cdc9857cf15e517d
Quinn Ngo <quinnngo@usc.edu>
2019-02-01 18:08:15 -0800

    Add drug delete actions and reducers redux (#32)

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,4 +1,6 @@
 export const UPDATE_WEEK = "UPDATE_WEEK";
-export const ADD_DRUG = "ADD_DRUG";
 export const ADD_REMINDER = "ADD_REMINDER";
 export const UPDATE_REMINDER = "UPDATE_REMINDER";
+
+export const ADD_DRUG = "ADD_DRUG";
+export const DELETE_DRUGS = "DELETE_DRUGS";
--- src/redux/reducers/drug.js
+++ src/redux/reducers/drug.js
@@ -5,6 +5,7 @@ let drugState = {
   drugInfo: testDrugs || [],
   drugId: 30
 };
+
 export default (drugInfoReducer = (state = drugState, action) => {
   switch (action.type) {
     case ADD_DRUG:
@@ -12,6 +13,12 @@ export default (drugInfoReducer = (state = drugState, action) => {
       const newDrugInfo = [...state.drugInfo, drug];
       state = { ...state, drugInfo: newDrugInfo };
       return state;
+    case DELETE_DRUGS:
+      const { drugIds } = action;
+      return {
+        ...state,
+        drugInfo: state.drugInfo.filter(drug => !drugIds.includes(drug.id));
+      }
     default:
       return state;
   }
