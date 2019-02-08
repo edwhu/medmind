5124433f173cba13cd42fdebf40d592808243094
ethan-yu0503 <ethanyu.code@gmail.com>
2019-02-07 23:46:38 -0800

    Completed Styling for Modal
    - styling is complete
    - refactored inline styling for components and placed styling attributes under the stylesheet
    - new icons from Jamie L
    
    Just need to change font in the future

--- src/components/DrugInEvent/DrugInEvent.js
+++ src/components/DrugInEvent/DrugInEvent.js
@@ -14,9 +14,9 @@ import CloseButton from "../../assets/button-skip.png"
 import SkipButton from "../../assets/button-skip2.png";
 import TakeButton from "../../assets/button-take2.png";
 import SnoozeButton from "../../assets/button-snooze2.png";
-import DosageIcon from "../../assets/icon-dosage.png";
+import DosageIcon from "../../assets/icon-dosage.jpg";
 import TimeIcon from "../../assets/icon-time.png";
-import NotesIcon from "../../assets/icon-notes.png";
+import NotesIcon from "../../assets/icon-notes.jpg";
 import EditIcon from "../../assets/icon-edit2.png"
 
 function getFadedFromHex(hexColor) {
@@ -29,6 +29,7 @@ function getFadedFromHex(hexColor) {
 }
 
 const createStyles = drugColor => {
+  const lighterColor = getFadedFromHex(drugColor);
   return StyleSheet.create({
     container: {
       width: 300,
@@ -54,15 +55,93 @@ const createStyles = drugColor => {
       fontWeight: "bold"
     },
     drugDosage: {},
+    modalContainer: {
+      justifyContent: 'space-between',
+      backgroundColor: 'white',
+      borderRadius: 20,
+      borderWidth: 1,
+      height:'60%', 
+    },
+    modalTopBar: { 
+      borderTopLeftRadius: 20, 
+      borderTopRightRadius:20, 
+      height: '15%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 20,
+      backgroundColor: lighterColor
+    },
+    pillContainer: {
+      borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+      width: Dimensions.get('window').width * 0.15,
+      height: Dimensions.get('window').width * 0.15,
+      backgroundColor: lighterColor,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    modalPillBar: {
+      backgroundColor: 'white',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 30,
+      height: '30%'
+    },
+    modalDrugName: {
+      fontSize: 20, 
+      fontWeight: 'bold'
+    },
+    infoBar: {
+      height: '30%',
+      alignItems: 'flex-start',
+      justifyContent: 'space-between'
+    },
+    bottomBar: {
+      backgroundColor: '#e3e3e3',
+      height: '17.5%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      borderBottomLeftRadius: 20, 
+      borderBottomRightRadius: 20,
+      padding: 20
+      // borderWidth: 2,
+    },
     imageStyle: {
       width: 40,
       height: 40,
 
     },
+    iconView: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
     iconStyle: {
-      padding: 15,
-      width: 25,
-      height: 25
+      // borderWidth: 1,
+      width: 40,
+      height: 40,
+      marginLeft: 20,
+    },
+    iconTextView: {
+      marginLeft: 20, 
+      width: '70%',
+      justifyContent: 'center'
+    }, 
+    iconText: {
+      fontSize: 15,
+    },
+    closeButton: {
+      width: 34, 
+      height: 34
+    },
+    skipButtonView: {
+      alignItems: 'center',
+      marginLeft: 20
+    },
+    takeButtonView: {
+      alignItems: 'center'
+    },
+    snoozeButtonView: {
+      alignItems: 'center',
+      marginRight: 20
     }
   });
 };
@@ -71,155 +150,95 @@ class DrugInEvent extends Component{
   // let styles = createStyles(drug.color);
   state = {
     modalVisible: false,
+    editFormVisible: false,
     styles: createStyles(this.props.drug.color)
   };
 
   setModalVisible = visible => {
     this.setState({modalVisible: visible});
-  }
+  };
 
   render(){
     const { drug } = this.props;
     const { styles, modalVisible } = this.state;
     const color = drug.color;
-    const lighterColor = getFadedFromHex(color);
+
     return (
         <View>
           <Modal
-            // animationType="slide"
             animationInTiming = {300}
             transparent={true}
             visible={this.state.modalVisible}
-            // presentationStyle="formSheet"
-            onRequestClose={() => {
-              console.log("modal is closed");
-              }
-            }
             >
-            <View style={{
-                  // flex: 1,
-                  justifyContent: 'space-between',
-                  // alignItems: 'stretch',
-                  backgroundColor: 'white',
-                  borderRadius: 20,
-                  // borderWidth: 2,
-                  // padding: 10,
-                  // width: '80%',
-                  height:'60%',
-                  
-                  }}>
+            <View style={styles.modalContainer}>
 
-                  <View style={{ 
-                      borderTopLeftRadius: 20, 
-                      borderTopRightRadius:20, 
-                      backgroundColor: lighterColor,
-                      height: '15%',
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                      padding: 20
-                      }}>
-                    
-
+                  <View style={styles.modalTopBar}>
                       <TouchableHighlight
                           onPress={() => {
                             this.setModalVisible(false);
                           }}>
-                          <Image source={CloseButton} style={{width: 34, height: 34}} />
+                          <Image source={CloseButton} style={styles.closeButton} />
                       </TouchableHighlight>
 
-                      <Image source={EditIcon} style={styles.imageStyle} />
+                      <TouchableHighlight
+                          onPress={() => {
+                            this.setEditFormVisible(true);
+                          }}>
+                          <Image source={EditIcon} style={styles.imageStyle} />
+                      </TouchableHighlight>
                       
                   </View> 
 
-                  <View style={{
-                    backgroundColor: 'white',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    padding: 30,
-                    // borderWidth: 2,
-                    height: '30%'
-                  }}>
+                  <View style={styles.modalPillBar}>
                     <TouchableHighlight
-                      style = {{
-                        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
-                        width: Dimensions.get('window').width * 0.15,
-                        height: Dimensions.get('window').width * 0.15,
-                        backgroundColor: lighterColor,
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                      }}
+                      style = {styles.pillContainer}
                       underlayColor = '#ccc'
                     >
                       <DrugIcon color={drug.color} />
                     </TouchableHighlight>
                     
-                    <Text style={{fontSize: 20, fontWeight: 'bold'}}>{drug.name}</Text>
+                    <Text style={styles.modalDrugName}>{drug.name}</Text>
                     
                   </View>
 
-                  <View style={{
-                      height: '30%',
-                      alignItems: 'flex-start',
-                      justifyContent: 'space-between'
-                      // borderWidth: 2,
-
-                  }}>
-                      <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between'
-                      }}>
+                  <View style={styles.infoBar}>
+                      <View style={styles.iconView}>
                         <Image source={TimeIcon} style={styles.iconStyle} />
-                        <View style={{padding: 5}}>
-                          <Text style={{fontSize: 15}}> Scheduled for 9:30 AM, today, Oct 8 (Hardcoded) </Text>
+                        <View style={styles.iconTextView}>
+                          <Text style={styles.iconText}
+                                numberOfLines={2}> 
+                                Scheduled for 9:30 AM adfkhakjsdhfj kahsdjkfhakjsh dfjkhaskjdh fkashdfkh askdfjh</Text>
                         </View>
                       </View>
 
-                      <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-evenly'
-                      }}>
-                        <Image source={DosageIcon} style={styles.iconStyle} />
-                        <View style={{padding: 5}}>
-                          <Text style={{fontSize: 15}}>{drug.dosage}</Text>
+                      <View style={styles.iconView}>
+                          <Image source={DosageIcon} style={styles.iconStyle} />
+                        <View style={styles.iconTextView}>
+                          <Text style={styles.iconText}>{drug.dosage}</Text>
                         </View>
-                        
                       </View>
 
-                      <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-evenly'
-                      }}>
+                      <View style={styles.iconView}>
                         <Image source={NotesIcon} style={styles.iconStyle} />
-                        <View style={{padding: 5}}>
-                          <Text style={{fontSize: 15}}> Additional Notes </Text>
+                        <View style={styles.iconTextView}>
+                          <Text style={styles.iconText}> Additional Notes </Text>
                         </View>
                       </View>
                   </View>
 
-                  <View style={{
-                      height: '15%',
-                      flexDirection: 'row',
-                      justifyContent: 'space-evenly',
-                      // borderWidth: 2,
-                  }}>
+                  <View style={styles.bottomBar}>
 
-                    <View style={{
-                      alignItems: 'center'
-                    }}>
+                    <View style={styles.skipButtonView}>
                       <Image source={SkipButton} style={styles.imageStyle} />
                       <Text> Skip </Text>
                     </View>
 
-                    <View style={{
-                      alignItems: 'center'
-                    }}>
+                    <View style={styles.takeButtonView}>
                       <Image source={TakeButton} style={styles.imageStyle} />
                       <Text> Take </Text>
                     </View>
 
-                    <View style={{
-                      alignItems: 'center'
-                    }}>
+                    <View style={styles.snoozeButtonView}>
                       <Image source={SnoozeButton} style={styles.imageStyle} />
                       <Text> Snooze </Text>
                     </View>
